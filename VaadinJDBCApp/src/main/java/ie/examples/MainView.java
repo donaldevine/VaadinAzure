5080c5d1d6e5d8b36fdea66c2ddb77d40a683a3d
Donal Devine <donaldevine@gmail.com>
2019-11-27 13:30:22 +0000

    Fixed issue with jdbc dependency in vaadin app

--- VaadinJDBCApp/src/main/java/ie/examples/MainView.java
+++ VaadinJDBCApp/src/main/java/ie/examples/MainView.java
@@ -2,13 +2,10 @@ package ie.examples;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
 
 /**
  * The main view contains a button and a click listener.
@@ -22,24 +19,25 @@ public class MainView extends VerticalLayout {
 
         Connection connection = null;
 
-        String connectionString = "jdbc:sqlserver://donaldbserver01.database.windows.net:1433;database=donaldb;user=donal@donaldbserver01;password=;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        String connectionString = "jdbc:sqlserver://donaldbserver01.database.windows.net:1433;database=donaldb;user=donal@donaldbserver01;password=[PasswordGoesHere];encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
+        VerticalLayout layout  = new VerticalLayout();
+        
                         
         try {
             
-            DriverManager.registerDriver(new SQLServerDriver());
-
             // Connect with JDBC driver to a database
             connection = DriverManager.getConnection(connectionString);
             // Add a label to the web app with the message and name of the database we connected to 
-            Notification notification = Notification.show("Connected to database: " + connection.getCatalog());
+            layout.add(new Label("Connected to database: " + connection.getCatalog()));
             
         } catch (Exception e) {
             // This will show an error message if something went wrong
-            Notification notification = Notification.show(e.getMessage());
+            
+            layout.add(e.getMessage());
             
         }
 
-        
+        add(layout );
     }
 }
